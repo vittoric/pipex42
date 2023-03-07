66f0f69b31d2fb2d43f10670de02baec6607f13e
Vicoder <93945847+vittoric@users.noreply.github.com>
2023-03-07 18:42:28 +0100

    como hacer pipex y no morir en el intento?

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: vcodrean <vcodrean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:31:48 by vcodrean          #+#    #+#             */
-/*   Updated: 2023/03/07 15:43:46 by vcodrean         ###   ########.fr       */
+/*   Updated: 2023/03/07 16:04:28 by vcodrean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	main(int argc, char **argv, char **envp)
 	if (argc > 1 && argv)
 		printf("There are args\n");
 	mycmds = ft_split(argv[1], ' ');
+	//verifica si el en el argv ya se ha pasado un PATH
 	ret = access(mycmds[0], X_OK);
      if (ret == 0)
     {
